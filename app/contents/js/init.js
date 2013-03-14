8e0861c0a6060f164f056b253261170e270f421b
Tessa Harmon <tessa@tessaharmon.com>
2013-03-13 22:37:43 -0400

    Adding more page templates and basic error handling.

--- app/contents/js/init.js
+++ app/contents/js/init.js
@@ -5,18 +5,28 @@
     api = require('api');
 
   var appVM = {
-    searchQuery: ko.observable()
+    searchQuery: ko.observable(),
+    page: ko.observable({}),
+    currentBeers: ko.observableArray([]),
+    errorMessages: ko.observableArray()
   }
 
+  // Brew Search form handler.
   appVM.searchBrews = function(form) {
+    var self = this;
+
     var q = $(form).find('#query').val();
 
     api.getBeers(q, function(err, res) {
       if (!err) {
-        console.log(res);
+        if (res.total > 0) {
+          self.errorMessages([]);
+          return self.currentBeers(res.beers);
+        }
+        return self.errorMessages.push("Sorry, no beer found. Try again.")
       }
+      self.errorMessages.push("There was an error processing your search. Please try again.");
     });
-
   };
 
   $.domReady(function() {
