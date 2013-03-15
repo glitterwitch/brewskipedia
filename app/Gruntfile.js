2929f9582c9d177b091686d7c976949bb231645a
Tessa Harmon <tessa@tessaharmon.com>
2013-03-15 09:58:46 -0400

    Make font size of heading less gargantuan for Mozilla

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,15 +1,30 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     meta: {},
-    uglify: {
-      build: {
-        src: ['contents/js/vendor/jquery.js', 'contents/js/vendor/knockout.js', 'contents/js/vendor/sammy.js' ],
+    min: {
+      vendor: {
+        src: [
+          'contents/js/vendor/radio.js',
+          'contents/js/vendor/ender.js',
+          'contents/js/vendor/knockout.js',
+          'contents/js/vendor/underscore-min.js',
+        ],
         dest: 'build/js/vendor.min.js'
+      },
+      app: {
+        src: [
+          'contents/js/config.js',
+          'contents/js/components/ui.js',
+          'contents/js/components/router.js',
+          'contents/js/components/api.js',
+          'contents/js/pages/SearchResults.js',
+          'contents/js/pages/BeerDetails.js',
+          'contents/js/init.js'
+        ],
+        dest: 'build/js/app.min.js'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', 'min');
 };
